caa69102d8626bfd9adc8e7c0c85c60b895fd83a
Aloder2006 <82025827+Aloder2006@users.noreply.github.com>
2022-08-11 01:20:28 +0300

    Add files via upload

---
+++ AjaxPush.js
@@ -0,0 +1,41 @@
+AjaxPush = function(listener, sender)
+{
+    this.listener = listener || "";
+    this.sender = sender || "";
+    this.state = false;
+    this.timestamp = 0;
+}
+
+AjaxPush.prototype = 
+{
+    connect: function(callback){
+        var that = this;
+        var status = false;
+
+        $.ajax({ url: this.listener, dataType: 'json',
+    data: { timestamp: this.timestamp},
+    success: function(data){
+        if(!that.state){
+            console.info("Connected!");
+        }
+        status = true;
+        this.state = true;
+        that.timestamp = data["timestamp"];
+        callback(data);
+    },
+    complete: function(data){
+        if(!status){
+            console.info("The connect has been lost");
+            setTimeout(function(){ that.connect(callback); }, 1000);
+        } else
+            that.connect(callback);
+
+        that.state = (data.status == 200) ? true : false;
+    }})
+    },
+    doRequest: function(data, callback){
+        $.ajax({ url: this.sender, data: data,
+            success: function(){ callback(); }
+        });
+    }
+}
